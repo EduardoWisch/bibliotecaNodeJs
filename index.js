c9601a10a781385f53478d32bc0abae9c161d909
EduardoWisch <eduardo.wisch@escola.pr.gov.br>
2024-07-07 11:06:10 -0300

    Conteúdo da aula 3

---
+++ index.js
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function extractLinks(text){
+  const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+  const catches = [...text.matchAll(regex)]
+  const results = catches.map(catches => ({[catches[1]]: catches[2]}))
+  return results
+}
+
+function handleError(error) {
+  console.log(error);
+  throw new Error(chalk.red(error.code, 'não há arquivo no diretório'));
+}
+
+// async/await
+
+async function getFileWithAsync(filePath) {
+  try {
+    const encoding = 'utf-8';
+    const text = await fs.promises.readFile(filePath, encoding)
+    console.log(extractLinks(text))
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+
+// promises com then()
+
+function getFileWithThen(filePath) {
+  const encoding = 'utf-8';
+  fs.promises
+    .readFile(filePath, encoding)
+    .then((text) => console.log(chalk.green(text)))
+    .catch(handleError)
+}
+
+getFileWithAsync('./arquivos/texto.md');
